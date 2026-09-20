f99b6734e08cb73980e112713796c42b00244a18
shippomokomo <326574533+shippomokomo@users.noreply.github.com>
2026-09-20 21:18:27 +0900

    Add illustrations gallery

---
+++ src/data/artists.ts
@@ -0,0 +1,16 @@
+export const artists = {
+  kikuri: {
+    name: 'きくり',
+    url: 'https://x.com/kikury_',
+  },
+
+  taidanuki: {
+    name: '怠惰ヌキ',
+    url: 'https://x.com/taidade_iijan',
+  },
+
+  macoftianmau: {
+    name: 'マコフシャン・マウ',
+    url: 'https://x.com/macofmau',
+  },
+};
