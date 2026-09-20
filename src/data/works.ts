cf7f2daaadd1477779ebd86660a8ee1cebad5c93
shippomokomo <326574533+shippomokomo@users.noreply.github.com>
2026-09-21 02:04:48 +0900

    Complete works page

---
+++ src/data/works.ts
@@ -0,0 +1,115 @@
+export const workSections = [
+  {
+    id: 'interviews',
+    title: 'Interviews',
+    works: [
+      {
+        title: 'INTERVIEW with Independent VTubers: Shippo Mokomo',
+        image: '2025_interview_yattatachi.jpg',
+        description:
+          '日本のポップカルチャーを紹介する英字メディア Yatta-Tachi 様から取材を受け、個人Vtuberとして紹介していただきました！個人Vtuberとして配信を始めたきっかけや、活動に対する姿勢などの質問に答えています！',
+        links: [
+          {
+            label: 'インタビュー記事',
+            url: 'https://yattatachi.com/interview-shippo-mokomo',
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 'games',
+    title: 'Games',
+    works: [
+      {
+        title: 'VtuberCommandBattLIVE (2025)',
+        image: '2025_game_vtubercommandbattlive.webp',
+        description:
+          '実在のVtuberさんが登場！ファンメイドのローグライト風コマンドバトルです。グッズとファンアートを集めてVTuberを強化し、交流を無事成功させ、金盾級配信者を目指しましょう！プレイアブルキャラクターとして出演させていただきました！',
+        links: [
+          {
+            label: 'ダウンロード：Booth',
+            url: 'https://tachibanakahito.booth.pm/items/7775043',
+          },
+          {
+            label: '制作：橘化人様',
+            url: 'https://x.com/kahito_mod',
+          },
+        ],
+      },
+
+      {
+        title: 'Ichima-san (2024)',
+        image: '2024_game_ichima-san.jpg',
+        description:
+          'かわいい市松人形となり、人に見つからないよう屋敷を探索する、ホラーゲームの皮を被った心温まるステルスアクションアドベチャー。イースターエッグ的な隠し要素としてゲスト出演させていただきました！',
+        links: [
+          {
+            label: 'ダウンロード：Steam',
+            url: 'https://store.steampowered.com/app/2416190/Ichimasan/',
+          },
+          {
+            label: 'ダウンロード：Nintendo Switch (JP)',
+            url: 'https://store-jp.nintendo.com/item/software/D70010000087284',
+          },
+          {
+            label: '紹介記事',
+            url: 'https://note.com/hayami_sewo/n/n038ea77b60d4',
+          },
+        ],
+      },
+
+      {
+        title: '幻創の塔 (2022)',
+        image: '2022_game_gensonoto.webp',
+        description:
+          '40名を超えるVtuberが登場するハクスラRPG。1周のプレイ時間が1～2時間と短いながらも、ランダムなマップ生成・入手アイテム等によりリプレイ性の高いゲーム。プレイアブルキャラクターとして出演させていただきました！リリース当時のライブ配信上での出来事をベースにした技を使用します！',
+        links: [
+          {
+            label: 'ダウンロード：ウディタ',
+            url: 'https://ux.getuploader.com/wolfRPGeditor/',
+          },
+          {
+            label: '制作：橘化人様',
+            url: 'https://x.com/kahito_mod',
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 'mods',
+    title: 'MODs',
+    works: [
+      {
+        title: 'MOKO IS YOU (2025)',
+        image: '2025_mod_mokoisyou.jpg',
+        description:
+          'パズルゲーム「BABA IS YOU」のもこもテーマのMODを作っていただきました！かなり歯ごたえのあるレベルが揃っています！',
+        links: [
+          {
+            label: 'ダウンロード：Ver 1.50b（最新版）',
+            url: 'https://drive.google.com/file/d/18lTT8QpneHTTSIDCMFIEat0ij7lorEbw/view?usp=sharing',
+          },
+        ],
+        notice:
+          '※ZIPファイルを解凍後、ReadMeをご確認ください。',
+      },
+
+      {
+        title: "Patrick's Parabox MOD (2023)",
+        image: '2023_mod_patricksparabox.webp',
+        description:
+          "パズルゲーム「Patrick's Parabox」のもこもテーマのMODを作っていただきました！アイデアの詰まった、愛のあふれるレベル全10問！",
+        links: [
+          {
+            label: 'ダウンロード（Google Drive）',
+            url: 'https://drive.google.com/file/d/1-Rpq_eGnthQHm7S_6fVkZ6k0Pzvd6abj/view?usp=sharing',
+          },
+        ],
+      },
+    ],
+  },
+];
