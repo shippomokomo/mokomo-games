b456672d18c7654468bb1b1449689e4c90cbed33
shippomokomo <326574533+shippomokomo@users.noreply.github.com>
2026-09-22 08:44:34 +0900

    Add illustration gallery content and video support

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -4,18 +4,348 @@ export const artists = {
     url: 'https://x.com/kikury_',
   },
 
-  taidanuki: {
+  nuki: {
     name: '怠惰ヌキ',
     url: 'https://x.com/taidade_iijan',
   },
 
-  macoftianmau: {
+  macof: {
     name: 'マコフシャン・マウ',
     url: 'https://x.com/macofmau',
   },
 
-  nekonominmi: {
+  minmi: {
     name: '猫野みんみ',
     url: 'https://x.com/MinMi_0701',
+  },
+
+  dango: {
+    name: 'Dango',
+    url: 'https://x.com/Dangokatsu',
+  },
+
+  ebimomo: {
+    name: 'エビ桃',
+    url: 'https://x.com/ebimomomo',
+  },
+
+  nueko: {
+    name: 'ヌエ子',
+    url: 'https://x.com/nueko829',
+  },
+
+  roromi: {
+    name: 'おがころろみ',
+    url: 'https://x.com/roronekorone',
+  },
+
+  kawarage: {
+    name: 'かわらげ',
+    url: 'https://x.com/kawarage_yatano',
+  },
+
+  suurin: {
+    name: 'すうりん',
+    url: 'https://x.com/suurin_2',
+  },
+
+  kappa: {
+    name: 'どっとかっぱ',
+    url: 'https://x.com/kappa_dot',
+  },
+
+  nitoro: {
+    name: 'nitoro',
+    url: 'https://x.com/torotoronitoro',
+  },
+
+  nyatelier: {
+    name: 'ニャトリエ',
+    url: 'https://x.com/Nyatelier_12',
+  },
+
+  hachio: {
+    name: 'はちお',
+    url: 'https://x.com/hachio81',
+  },
+
+  hachi: {
+    name: 'はち',
+    url: 'https://x.com/heihati',
+  },
+
+  mametanuki: {
+    name: 'まめたぬき',
+    url: 'https://x.com/mametanukiroom0',
+  },
+
+  miyanokoko: {
+    name: 'みやのここ',
+    url: 'https://x.com/miyanokoko8',
+  },
+
+  mofuaki: {
+    name: 'もふあき',
+    url: 'https://x.com/mofuaki',
+  },
+
+  yonekazu: {
+    name: 'YONEKAZU',
+    url: 'https://x.com/yonekazuri',
+  },
+
+  nmo: {
+    name: 'ンモ=エザゴ',
+    url: 'https://x.com/nomeazog',
+  },
+
+  kogarasumaru: {
+    name: '小烏丸',
+    url: 'https://x.com/kokarasuma',
+  },
+
+  tsuko: {
+    name: '色辺つこ',
+    url: 'https://x.com/shikibetsuko',
+  },
+
+  asaneminato: {
+    name: '朝寝みなと',
+    url: 'https://x.com/asane_minato',
+  },
+
+  gami: {
+    name: '死神のガミ',
+    url: 'https://x.com/gami_TPH',
+  },
+
+  kuroki: {
+    name: 'Biahsh Kuroki',
+    url: 'https://x.com/Kinoko_Akane',
+  },
+
+  imonyaki: {
+    name: 'ヤキイモン',
+    url: 'https://x.com/Yaki_Arts',
+  },
+
+  kasu: {
+    name: 'KasuGMond',
+    url: 'https://x.com/KasuGMond',
+  },
+
+  pumo: {
+    name: 'Pumopumoshi',
+    url: 'https://x.com/pumopumoshi',
+  },
+
+  purath: {
+    name: 'Purath',
+    url: 'https://x.com/purarthh',
+  },
+
+  yamawoods: {
+    name: 'Yama Woods',
+    url: '',
+  },
+
+  zahashi: {
+    name: 'Zahashi',
+    url: 'https://x.com/DZahashi',
+  },
+
+  anmitsu: {
+    name: 'あんみつ',
+    url: 'https://x.com/anmitsu46',
+  },
+
+  emuyama: {
+    name: 'えむ山りし夫',
+    url: 'https://x.com/emu_risi',
+  },
+
+  kinchan: {
+    name: '木ノ実きん',
+    url: 'https://x.com/KinChan61211',
+  },
+
+  kurokuma: {
+    name: '希輝',
+    url: 'https://x.com/kurojump1',
+  },
+
+  gekowanko: {
+    name: 'ゲコわんこ',
+    url: 'https://x.com/Geko_wanko',
+  },
+
+  konu: {
+    name: '狸野こぬ',
+    url: 'https://x.com/konothama',
+  },
+
+  damaki: {
+    name: '髙井田マキ',
+    url: 'https://x.com/Damaki_rail',
+  },
+
+  terakawanufu: {
+    name: 'テラカワヌフ',
+    url: 'https://x.com/kil_ling_t_idol',
+  },
+
+  tomokadon: {
+    name: 'ともかどん',
+    url: 'https://x.com/tomokadon',
+  },
+
+  nano: {
+    name: 'みやたなの',
+    url: 'https://x.com/miyatasanthi',
+  },
+
+  nusutto: {
+    name: 'ヌスット',
+    url: 'https://x.com/nucchonucho',
+  },
+
+  shijimi: {
+    name: 'ノノしじみ',
+    url: 'https://x.com/nono_shijimi',
+  },
+
+  banirisu: {
+    name: 'バニリスフロウ',
+    url: 'https://x.com/vanishcele',
+  },
+
+  feimori: {
+    name: 'フェイ・モーリ',
+    url: 'https://x.com/feimmo20',
+  },
+
+  maxima: {
+    name: 'MAXIMA',
+    url: 'https://x.com/maxima_illust',
+  },
+
+  meltzmornir: {
+    name: 'メルツ・モルニル',
+    url: 'https://x.com/Meltzmornir52',
+  },
+
+  yabee: {
+    name: 'やべえ',
+    url: 'https://x.com/subbeesan',
+  },
+
+  romu: {
+    name: '化模狐浪夢',
+    url: 'https://x.com/kemokemo6262',
+  },
+
+  watou: {
+    name: 'ワトウ',
+    url: 'https://x.com/watou12',
+  },
+
+  ruma: {
+    name: 'Ruma',
+    url: 'https://x.com/MngkhlRng',
+  },
+
+  kahito: {
+    name: '橘化人',
+    url: 'https://x.com/kahito_mod',
+  },
+
+  buchi: {
+    name: '九尾乃猫ブチ',
+    url: 'https://x.com/Kubinonekomcef',
+  },
+
+  kurutsumu: {
+    name: '胡桃つむぎ',
+    url: 'https://x.com/tmgi_krmi',
+  },
+
+  goma: {
+    name: '胡麻',
+    url: 'https://x.com/g0madang080',
+  },
+
+  mikikageshu: {
+    name: '御樹景柊',
+    url: 'https://x.com/Mikikage_Syu',
+  },
+
+  mondoro: {
+    name: '甲乃もんどろうさん',
+    url: 'https://www.pixiv.net/users/18427817',
+  },
+
+  takatoki: {
+    name: '高時',
+    url: '',
+  },
+
+  crowraul: {
+    name: '黒羽ラウル',
+    url: 'https://x.com/CrowraulElf',
+  },
+
+  samaa: {
+    name: '沙真愛',
+    url: '',
+  },
+
+  popura: {
+    name: '七缶ぽぷら',
+    url: 'https://x.com/poplar_nanakama',
+  },
+
+  kokuu: {
+    name: '墟空',
+    url: 'https://x.com/range3000km',
+  },
+
+  yukimoriyomiko: {
+    name: '雪森よみこ',
+    url: '',
+  },
+
+  hidaka: {
+    name: '日高',
+    url: 'https://x.com/hidaka_mu',
+  },
+
+  hiyuki: {
+    name: '十五月氷雪',
+    url: 'https://x.com/mochiLunahiyuki',
+  },
+
+  yotsuba: {
+    name: '平成よつば',
+    url: 'https://x.com/ytb581',
+  },
+
+  kitaki: {
+    name: '北木つね',
+    url: 'https://x.com/v_kitaki',
+  },
+
+  komari: {
+    name: '旅猫こまり',
+    url: 'https://x.com/v_tabineco',
+  },
+
+  rinvtuber: {
+    name: '凛',
+    url: 'https://x.com/rinvtuber7',
+  },
+
+  fukurou: {
+    name: '天空梟',
+    url: 'https://x.com/Amazora_owl_02',
   },
 };
