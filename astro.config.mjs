7ea8e151e7b694f6bfeb4d8771840060c36234fc
shippomokomo <326574533+shippomokomo@users.noreply.github.com>
2026-09-23 02:25:34 +0900

    Prepare site for www.mokomo.games

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shippomokomo.github.io',
-  base: '/mokomo-games',
+  site: 'https://www.mokomo.games',
+  base: '/',
 });
